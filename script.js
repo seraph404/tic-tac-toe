e98b0e35b0287fecb61e312a304e06a454731a46
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-26 11:33:00 -0400

    Create initializeGame function

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const GameBoard = (() => {
   const board = [];
 
   return {
+    // this creates the board array
     createBoard: function () {
+      // creates the gameboard 2D array
       let rows = 3;
       let columns = 3;
-
-      // creating two-dimensional array
       for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
@@ -14,11 +14,28 @@ const GameBoard = (() => {
         }
       }
 
-      console.log(board);
+      return board;
+    },
+    displayBoard: function () {
+      for (let row of board) {
+        console.log(row.map((cell) => (cell === 0 ? "-" : cell)).join(" | "));
+      }
     },
   };
 })();
 
 const createPlayer = (name, marker) => {
   return { name, marker };
+};
+
+const initializeGame = (name) => {
+  let playerTwoMarker;
+  const player1 = createPlayer(name, "X");
+  playerTwoMarker = player1.marker === "X" ? "O" : "X";
+  const player2 = createPlayer("Player 2", playerTwoMarker);
+
+  return {
+    board: GameBoard,
+    players: [player1, player2],
+  };
 };
