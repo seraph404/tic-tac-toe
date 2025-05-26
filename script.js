b4b27846767b4205945a9aefc06d90fc45919aa7
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-26 11:19:21 -0400

    Create player factory

--- script.js
+++ script.js
@@ -22,25 +22,3 @@ const GameBoard = (() => {
 const createPlayer = (name, marker) => {
   return { name, marker };
 };
-
-const initializeGame = (name) => {
-  let playerTwoMarker;
-  const player1 = createPlayer(name, "X");
-  playerTwoMarker = player1.marker === "X" ? "O" : "X";
-  const player2 = createPlayer("Player 2", playerTwoMarker);
-
-  return {
-    board: GameBoard,
-    players: [player1, player2],
-  };
-};
-
-const welcomeText = (() => {
-  console.log("====================");
-  console.log("Welcome to Tic Tac Toe!");
-  console.log("To begin, you can use the initializeGame function.");
-  console.log(
-    "You'll want to provide your name and the marker you'd like to use as arguments."
-  );
-  console.log("Like this: initializeGame('Seraphina', 'X')");
-})();
