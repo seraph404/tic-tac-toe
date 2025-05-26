49d711f2d8f307bce1e44f5eb46e0f6a49770131
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-26 11:40:42 -0400

    Create welcome text IIFE

--- script.js
+++ script.js
@@ -29,13 +29,27 @@ const createPlayer = (name, marker) => {
 };
 
 const initializeGame = (name) => {
+  // identify player2 marker based on input
   let playerTwoMarker;
   const player1 = createPlayer(name, "X");
   playerTwoMarker = player1.marker === "X" ? "O" : "X";
   const player2 = createPlayer("Player 2", playerTwoMarker);
+
+  GameBoard.createBoard();
+  GameBoard.displayBoard();
 
   return {
     board: GameBoard,
     players: [player1, player2],
   };
 };
+
+const welcomeText = (() => {
+  console.log("====================");
+  console.log("Welcome to Tic Tac Toe!");
+  console.log("To begin, you can use the initializeGame function.");
+  console.log(
+    "You'll want to provide your name and the marker you'd like to use as arguments."
+  );
+  console.log("Like this: initializeGame('Seraphina', 'X')");
+})();
