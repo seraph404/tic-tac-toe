3e7a670cdc4798280292ee9360f95e22b124bdcf
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-30 18:14:17 -0400

    Create updateGameboard to update board array, wrapped GameBoard function in an IIFE

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function GameBoard() {
+const GameBoard = (() => {
   const GRID_DIMENSION = 3;
   // let board = [];
 
+  // make createBoard()
   // for (let y = 0; y < GRID_DIMENSION; y++) {
   //   board[y] = [];
   //   for (let x = 0; x < GRID_DIMENSION; x++) {
@@ -17,16 +18,32 @@ function GameBoard() {
 
   function displayGameboard() {
     console.log(`==== GAMEBOARD ====`);
+    let num = 1;
     // loop through each row in the board
     board.forEach((row) => {
       // display that row in the console
-      console.log(`      |${row.join("|")}|`);
+      console.log(`${num}     |${row.join("|")}|`);
+      num += 1;
     });
+  }
+
+  function updateGameboard(move, marker) {
+    // account for zero index
+    const row = move[0] - 1;
+    const col = move[1] - 1;
+
+    // add checks here later OR do input sanitization eventually
+    board[row][col] = marker;
+    displayGameboard();
   }
 
   return {
     displayGameboard,
+    updateGameboard,
   };
-}
+})();
 
-GameBoard().displayGameboard();
+//GameBoard().displayGameboard();
+
+GameBoard.displayGameboard();
+GameBoard.updateGameboard([2, 2], "X");
