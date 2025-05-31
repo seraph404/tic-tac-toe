13d1dd6e1c8c4cfcea7bf3b7caf8dedab8d29ceb
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-30 20:21:18 -0400

    Create checkForWinner function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const GameBoard = (() => {
   const GRID_DIMENSION = 3;
-  let board = [];
 
+  let board = [];
   function createGameboard() {
     for (let y = 0; y < GRID_DIMENSION; y++) {
       board[y] = [];
@@ -10,12 +10,6 @@ const GameBoard = (() => {
       }
     }
   }
-
-  // let board = [
-  //   ["_", "X", "O"],
-  //   ["O", "_", "X"],
-  //   ["_", "O", "X"],
-  // ];
 
   function displayGameboard() {
     console.log(`==== GAMEBOARD ====`);
@@ -38,15 +32,47 @@ const GameBoard = (() => {
     displayGameboard();
   }
 
+  function checkForWinner() {
+    // prettier-ignore
+    // map out winning combos via index
+    const winningRows = [
+      [[0, 0], [0, 1], [0, 2]], // top row
+      [[1, 0], [1, 1], [1, 2]], // middle row
+      [[2, 0], [2, 1], [2, 2]], // bottom row
+      [[0, 0], [1, 0], [2, 0]], // left column
+      [[0, 1], [1, 1], [2, 1]], // middle column
+      [[0, 2], [1, 2], [2, 2]], // right column
+      [[0, 0], [1, 1], [2, 2]], // diagonal bottom-top
+      [[0, 2], [1, 1], [2, 0]], // diagonal top-bottom
+  ];
+
+    // loop over each combination in the array
+    winningRows.forEach((combo) => {
+      const [a, b, c] = combo.map(([row, col]) => board[row][col]);
+      // look for a full line of matching non-empty characters
+      if (a !== "_" && a === b && b === c) {
+        // marker wins!
+        console.log(`${a} wins!`);
+      }
+    });
+  }
+
   return {
     createGameboard,
     displayGameboard,
     updateGameboard,
+    checkForWinner,
   };
 })();
 
+function initializeGame() {
+  return {};
+}
+
 //GameBoard().displayGameboard();
 
-GameBoard.createGameboard();
-GameBoard.displayGameboard();
-GameBoard.updateGameboard([2, 2], "X");
+// GameBoard.createGameboard();
+// GameBoard.displayGameboard();
+//GameBoard.checkForWinner();
+// GameBoard.updateGameboard([2, 2], "X");
+//initializeGame();
